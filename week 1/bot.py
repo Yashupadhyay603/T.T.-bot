c28d697f2a67e6a3a8ffafd7a73b3da74c827513
Yashupadhyay603 <57247371+Yashupadhyay603@users.noreply.github.com>
2020-12-24 13:15:08 +0530

    Add files via upload

--- week 1/bot.py
+++ week 1/bot.py
@@ -5,9 +5,28 @@ import cv2
 import numpy as np
 import math
 
+def simple():
+    x=np.random.randint(3,9)/10
+    y=np.random.randint(-4,4)/10
+    puck_pos = [x, y, 0.30]
+    puck_ori = p.getQuaternionFromEuler([0, 0, 0])
+    puck = p.loadURDF("puck(1).urdf", puck_pos, puck_ori)
+    vel=np.random.randint(-20,0)/10
+    p.resetBaseVelocity(puck,[vel,0,0])
+
+def notsosimple():
+    x = np.random.randint(3, 9) / 10
+    y = np.random.randint(-4, 4) / 10
+    puck_pos = [x, y, 0.30]
+    puck_ori = p.getQuaternionFromEuler([0, 0, 0])
+    puck = p.loadURDF("puck(1).urdf", puck_pos, puck_ori)
+    vel_x=np.random.randint(-20,0)/10
+    vel_y=np.random.randint(-20, 0) / 10
+    p.resetBaseVelocity(puck, [vel_x, vel_y, 0])
 
 def getcentre(image):
     center = ()
+    centorg = None
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     mask = cv2.inRange(gray, 10, 255)
     contours = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
@@ -21,7 +40,6 @@ def getcentre(image):
                 centorg = ((center[0] - image.shape[1] / 2), (center[1] - image.shape[
                     0] / 2))  # shifting the origin to centre of table and then locating centre of puck
     return centorg
-
 
 # setting physics client
 PhysicsClent = p.connect(p.GUI)
@@ -38,63 +56,59 @@ table_pos = [0, 0, 0]
 table_ori = p.getQuaternionFromEuler([0, 0, 0])
 table = p.loadURDF("table(1).urdf", table_pos, table_ori)
 
-# setting puck
-puck_pos = [-0.6, 0.3, 0.3]
-puck_ori = p.getQuaternionFromEuler([0, 0, 0])
-puck = p.loadURDF("puck(1).urdf", puck_pos, puck_ori)
-# hitbot
-
-hitbot_pos=[0.41,0,0.31]
-hitbot_ori=p.getQuaternionFromEuler([0,0,3.14])
-hitbot=p.loadURDF("hitbot.urdf",hitbot_pos,hitbot_ori)
-
-
 # setting kuka bot
 kuka = p.loadURDF("model.urdf", [-1.0, 0, 0], p.getQuaternionFromEuler([0, 0, 0]))
 
 # creating constraints for table
 p.createConstraint(plane, -1, table, -1, p.JOINT_FIXED, [0, 0, 0], table_pos, [0, 0, 0])
-p.createConstraint(plane,-1,hitbot,-1, p.JOINT_FIXED,[0,0,0],hitbot_pos,[0,0,0],hitbot_ori)
 
+time.sleep(1)
+simple()
+#notsosimple()
 ALPHA = 1
 centrepoints = []
 timestamps = []
 for i in range(10000):
+    #p.setRealTimeSimulation(1)
     p.stepSimulation()
 
-    puck_pos, puck_ori = p.getBasePositionAndOrientation(puck)
-
-    force = ALPHA * (np.array([2.05, -3.0, puck_pos[2]]) - np.array(puck_pos))
-    p.applyExternalForce(objectUniqueId=puck, linkIndex=-1,
-                         forceObj=force, posObj=puck_pos, flags=p.WORLD_FRAME)
-
+     # #puck_pos, puck_ori = p.getBasePositionAndOrientation(puck)
+    #
+    #
     # setting camera
     view = p.computeViewMatrix([0, 0, 1.4], [0, 0, 0.5], [0, 1, 0])
-    projection = p.computeProjectionMatrixFOV(90, 1, 0.1, 3.1)
 
-    # getting images
-    images = p.getCameraImage(512, 512, viewMatrix=view, projectionMatrix=projection)
+    projection = p.computeProjectionMatrixFOV(90, 1, 0.1, 3.1)
+    #p.stepSimulation()
+    #
+    # # getting images
+    images = p.getCameraImage(112, 112, viewMatrix=view, projectionMatrix=projection)
+    p.stepSimulation()
     rgb=images[2]
     rgb= np.array(rgb)
-    rgb= np.reshape(rgb,(512,512,4))
+    # #p.stepSimulation()
+    rgb= np.reshape(rgb,(112,112,4))
     rgb= np.uint8(rgb)
+    p.stepSimulation()
     image= cv2.cvtColor(rgb,cv2.COLOR_BGR2RGB)
+    p.stepSimulation()
     centrec = getcentre(image)
-
+    p.stepSimulation()
+    #
     centrepoints.append(centrec)  # append all centres in a list
     timestamps.append(time.time())  # time at which the centre position was recorded appended into this list
     if (i % 2 == 0 and i != 0):
         print(centrec)
-        dx = centrepoints[i - 2][0] - centrepoints[i][
-            0]  # difference in x-coord of the two centre positions at i and i-2
-        dy = centrepoints[i - 2][1] - centrepoints[i][
-            1]  # difference in y-coord of the two centre positions at i and i-2
+        dx = centrepoints[i - 2][0] - centrepoints[i][0]  # difference in x-coord of the two centre positions at i and i-2
+        dy = centrepoints[i - 2][1] - centrepoints[i][1]  # difference in y-coord of the two centre positions at i and i-2
         distance = math.sqrt(abs(dx) * abs(dx) + abs(dy) * abs(dy))  # distance btw the two centres
         print(distance)
         velocity = distance / abs(timestamps[i] - timestamps[i - 2])  # speed calc using speed=distance/time
         print(velocity)
         angle = math.degrees(math.atan2(dy, dx))  # angle from the x-axis
         print("angle =", angle)
-    time.sleep(1. / 1000.)
+
+
+    time.sleep(1. / 240.)
 
 p.disconnect()
