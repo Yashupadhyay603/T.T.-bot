9b4939eb65c15fb0022c1c4cac9e6cbfc9e014b3
Yashupadhyay603 <57247371+Yashupadhyay603@users.noreply.github.com>
2021-01-03 19:53:53 +0530

    Add files via upload

--- week 1/botnew.py
+++ week 1/botnew.py
@@ -4,6 +4,7 @@ import time
 import cv2
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 
 def simple():
@@ -24,8 +25,8 @@ def notsosimple():
     puck_pos = [x, y, 0.32]
     puck_ori = p.getQuaternionFromEuler([0, 0, 0])
     puck = p.loadURDF("puck(1).urdf", puck_pos, puck_ori)
-    vel_x = np.random.randint(-20, 0) / 10
-    vel_y = np.random.randint(-20, 0) / 10
+    vel_x = np.random.randint(-60, -50) / 10
+    vel_y = np.random.randint(-20, 20) / 10
     vel = math.sqrt(vel_x * vel_x + vel_y * vel_y)
     p.resetBaseVelocity(puck, [vel_x, vel_y, 0])
     print("given velocity", vel)
@@ -36,10 +37,10 @@ def getcentre(image):
     center = ()
     centorg = None
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    mask = cv2.inRange(gray, 10, 255)
+    mask = cv2.inRange(gray, 5, 255)
     contours = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
     for c in contours:
-        if cv2.contourArea(c) < 300:
+        if cv2.contourArea(c) < 400:
             x,y,w,h = cv2.boundingRect(c)
             M = cv2.moments(c)
             if w != 0:
@@ -49,7 +50,7 @@ def getcentre(image):
                 # cv2.waitKey(0)
                 centorg = ((center[0] - (image.shape[1] / 2)), (center[1] - (image.shape[
                     0] / 2)))  # shifting the origin to centre of table and then locating centre of puck
-    return centorg
+    return center
 
 
 def calculatevelocity(i, image, puck):
@@ -86,28 +87,28 @@ def calculatevelocity(i, image, puck):
         return None
 
 
-def cal_pos_defend(position,velocity,angle):
-    x_vel=velocity*math.cos(math.radians(angle))
-    y_vel=velocity*math.sin(math.radians(angle))
-    x_pos=-x_vel+position[0]
-    y_pos=0
-    time=0.35
-    while(1):
-        t=0
-        if(y_vel<0):
-            t=(0.5+position[1])/y_vel
-            y_vel=-y_vel
-        elif(y_vel>0):
-            t=(0.5-position[1])/y_vel
-            y_vel=-y_vel
-        if t<time:
-            time=time-t
-        else:
-            if(y_vel>0):
-                y_pos=(0.5-time*y_vel)
-            elif(y_vel<0):
-                y_pos=(-0.5-time*y_vel)
-            return x_pos,y_pos
+def cal_pos_defend(position1,position2):
+    y=position1[1]+(position2[1]-position1[1])/(position2[0]-position1[0])*(40-position1[0])
+    while(y<67 and y>186):
+        if(y<67):
+            dif=68-y
+            y=68+dif
+        elif(y>186):
+            dif=y-186
+            y=186-dif
+    if(y<125):
+        y=(-0.5)/(125-67)*(y-125)
+    elif y>125:
+        y = (0.5) / (125 - 185) * (y - 125)
+    else:
+        y=0
+    return y
+
+
+
+
+
+
 
 
 
@@ -144,6 +145,7 @@ p.changeDynamics(table, 5, restitution=1)
 # creating constraints for table
 p.createConstraint(plane, -1, table, -1, p.JOINT_FIXED, [0, 0, 0], table_pos, [0, 0, 0])
 p.createConstraint(table,-1,hitbot,-1, p.JOINT_FIXED,[0,0,0],hitbot_pos,[0,0,0],hitbot_ori)
+p.setJointMotorControl2(hitbot, jointIndex=1, controlMode=p.POSITION_CONTROL, targetPosition=-.8, maxVelocity=1)
 
 
 time.sleep(1)
@@ -154,24 +156,47 @@ p.changeDynamics(puck, -1, restitution=1)
 ALPHA = 1
 centrepoints = []
 timestamps = []
+pos=[]
 for i in range(10000):
     p.setRealTimeSimulation(1)
     p.setTimeStep(1.0/240.0)
     # setting camera
     view = p.computeViewMatrix([0, 0, 1.4], [0, 0, 0.5], [0, 1, 0])
     projection = p.computeProjectionMatrixFOV(90, 1, 0.1, 3.1)
-    images = p.getCameraImage(112, 112, viewMatrix=view, projectionMatrix=projection)
+    images = p.getCameraImage(256, 256, viewMatrix=view, projectionMatrix=projection)
+
     rgb = images[2]
     rgb = np.array(rgb)
-    rgb = np.reshape(rgb, (112, 112, 4))
-    rgb = np.uint8(rgb)
-    image = cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB)
-    if(calculatevelocity(i,image,puck)!=None):
+    rgb = np.reshape(rgb, (256, 256, 4))
+    # plt.imshow( rgb)
+    # plt.show()
 
-        velocity, centre, angle = calculatevelocity(i, image, puck)
-        x_pos,y_pos=cal_pos_defend(centre,velocity,angle)
-        print(velocity," ",centre, " ", angle)
+
+    rgb = np.uint8(rgb)
+
+
+    image = cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB)
+
+    #pos,ori=p.getBasePositionAndOrientation(puck)
+
+    #if(math.fabs(math.tan(ori[2]))>0):
+    pos1 = getcentre(image)
+    pos.append(pos1)
+    # print(pos1)
+    print(pos)
+    if len(pos)==2:
+
+        y_pos = cal_pos_defend(pos[0],pos[1])
+
+            # print(y_pos)
         p.setJointMotorControl2(hitbot, jointIndex=2, controlMode=p.POSITION_CONTROL, targetPosition=y_pos, maxVelocity=1)
-        p.setJointMotorControl2(hitbot, jointIndex=1, controlMode=p.POSITION_CONTROL, targetPosition=x_pos, maxVelocity=1)
+        pos.clear()
+
+    # time.sleep(0.1)
+    # print(centre[1])
+    # p.setJointMotorControl2(hitbot, jointIndex=2, controlMode=p.POSITION_CONTROL, targetPosition=centre[1],
+    #                         maxVelocity=1.5)
+    #     #p.setJointMotorControl2(hitbot, jointIndex=2, controlMode=p.POSITION_CONTROL, targetPosition=y_pos, maxVelocity=2)
+
 
 p.disconnect()
